9849af752c4bca96cce02d71ad5ccb364a40f3c0
Kacper Grabiec <grabiec.kacper01@gmail.com>
2025-12-01 22:52:28 +0100

    day 1 part 1 completed

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,36 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var dial = 50
+        var dial0Counter = 0
+
+        input.forEach { line ->
+            val number: Int = (line.substring(startIndex = 1)
+                .toInt())
+
+            when (line[0]) {
+                'L' -> dial -= number
+                'R' -> dial += number
+            }
+
+            dial %= 100
+
+            if (dial == 0) {
+                dial0Counter++
+            }
+        }
+
+        return dial0Counter
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
+    part1(testInput).println()
     part1(input).println()
-    part2(input).println()
 }
